7941cdaace5ace80ad7fab97b5c064b7326fa140
vyagi <marcin.jagiela@hotmail.com>
2019-11-07 20:25:34 +0100

    IsPrime method implemented

---
+++ ClassesNo4/BasicMethods/MathUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    public static class MathUtilities
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            
+            for (int i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ ClassesNo4/BasicMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Math;
+namespace BasicMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"-10 {MathUtilities.IsPrime(-10)}");
+            Console.WriteLine($"0 {MathUtilities.IsPrime(0)}");
+            Console.WriteLine($"1 {MathUtilities.IsPrime(1)}");
+            Console.WriteLine($"2 {MathUtilities.IsPrime(2)}");
+            Console.WriteLine($"4 {MathUtilities.IsPrime(4)}");
+            Console.WriteLine($"7 {MathUtilities.IsPrime(7)}");
+            Console.WriteLine($"23 {MathUtilities.IsPrime(23)}");
+
+        }
+    }
+}
