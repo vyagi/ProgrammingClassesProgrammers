b1855f0b60fbe8c2ebcac6ff11b98bf20c6d4ae2
vyagi <marcin.jagiela@hotmail.c>
2019-11-28 19:19:06 +0100

    First approach

---
+++ ObjectOrientedProgramming/BasicClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.firstName = "Roman";
+            p1.lastName = "Kowalski";
+            p1.age = 20;
+
+            Console.WriteLine($"{p1.firstName} {p1.lastName} is {p1.age}");
+        }
+    }
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+}
