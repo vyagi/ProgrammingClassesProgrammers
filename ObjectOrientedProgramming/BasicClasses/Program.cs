526926e99650f798d3b38d1d19c53dc6a51ce7a8
vyagi <marcin.jagiela@hotmail.c>
2019-11-28 19:49:22 +0100

    Property initializer approach

--- ObjectOrientedProgramming/BasicClasses/Program.cs
+++ ObjectOrientedProgramming/BasicClasses/Program.cs
@@ -6,18 +6,38 @@ namespace BasicClasses
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person();
-            p1.firstName = "Roman";
-            p1.lastName = "Kowalski";
-            p1.age = 20;
-
-            Console.WriteLine($"{p1.firstName} {p1.lastName} is {p1.age}");
+            Person p1 = new Person("Janusz", "Wolny", -40);
+            
+            
+            Console.WriteLine($"{p1.GetFirstName()} {p1.GetLastName()} is {p1.GetAge()}");
         }
     }
     class Person
     {
-        public string firstName;
-        public string lastName;
-        public int age;
+        private string firstName;
+        private string lastName;
+        private int age;
+        public Person(string firstName, string lastName, int age)
+        {
+            if (firstName == null || lastName == null)
+                throw new ArgumentNullException("Names cannot be null");
+            this.firstName = firstName;
+            this.lastName = lastName;
+            if (age <= 0)
+                throw new ArgumentOutOfRangeException("Age must be postive");
+            this.age = age;
+        }
+        public string GetFirstName()
+        {
+            return this.firstName;
+        }
+        public string GetLastName()
+        {
+            return this.lastName;
+        }
+        public int GetAge()
+        {
+            return this.age;
+        }
     }
 }
