b12fb6b18371f74e1bc3961a3e128f2f12539e20
vyagi <marcin.jagiela@hotmail.c>
2019-12-18 18:41:00 +0100

    Done

---
+++ LanguageFeatures/Geometry/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace Geometry
+{
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ LanguageFeatures/Structs/Program.cs
@@ -0,0 +1,32 @@
+﻿using Geometry;
+using System;
+//using Jagiela;
+using Vector3D;
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point
+            {
+                X = 1,
+                Y = 3
+            };
+            Reflect(p1);
+            Console.WriteLine($"{p1.X} {p1.Y}");
+            //LordOfTheRings lotr = new LordOfTheRings();
+            //lotr.ShowYourself();
+            Vector v1 = new Vector(1, 1, 0);
+            Vector v2 = new Vector(-1, 1, 0);
+            var angle = Vector.AngleBetween(v1, v2);
+
+            Console.WriteLine(angle);
+        }
+        static void Reflect(Point p)
+        {
+            p.X = -p.X;
+            p.Y = -p.Y;
+        }
+    }
+}
