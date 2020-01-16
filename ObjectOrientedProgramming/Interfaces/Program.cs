523c505e1abc4bd176fbafcfacff2ebf86c0f1e2
vyagi <marcin.jagiela@hotmail.c>
2020-01-16 19:11:06 +0100

    Basic interfaces

---
+++ ObjectOrientedProgramming/Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+    public class Vehicle : IDrawable
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Imagine a picture of a vehicle");
+        }
+    }
+    public class Square : IDrawable
+    {
+        public void Draw()
+        {
+            Console.WriteLine("_____");
+            Console.WriteLine("_   _");
+            Console.WriteLine("_   _");
+            Console.WriteLine("_____");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDrawable[] thingsToDraw =
+            {
+                new Square(),
+                new Vehicle()
+            };
+
+            foreach (var item in thingsToDraw)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
