e35b94208deb020f0eaaf57affc89d6d2c366107
vyagi <marcin.jagiela@hotmail.c>
2020-01-17 17:47:53 +0100

    Reading a file

---
+++ WinForms/InvoiceApplication/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace InvoiceApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readButton = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(13, 13);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "Read file";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(13, 43);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(775, 394);
+            this.resultListBox.TabIndex = 1;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(95, 13);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(176, 13);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.readButton);
+            this.Name = "Form1";
+            this.Text = "Invoice manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ WinForms/InvoiceApplication/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InvoiceApplication
+{
+    public partial class Form1 : Form
+    {
+        private string _path = @"C:\Users\MJAGIELA\Desktop\Invoice1.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            statusLabel.Text = "Press Read file to read a file";
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            var content = File.ReadAllLines(_path);
+        }
+    }
+}
